8769632befd0b58f0a2462632e1cc9ec35306aee
WSINTRA <will.simpson85@gmail.com>
2019-05-23 15:15:40 -0400

    login creation working in backend and front

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,6 @@ class AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
  
   def create
-    
     @user = User.find_by(username: user_login_params[:username])
     #User#authenticate comes from BCrypt
     if @user && @user.authenticate(user_login_params[:password])
