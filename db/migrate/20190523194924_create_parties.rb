ff8d9176d89e8da67992fba1735f03e5a883b474
beschwa <naraiswar@gmail.com>
2019-05-23 17:11:35 -0400

    added description to parties table

--- db/migrate/20190523194924_create_parties.rb
+++ db/migrate/20190523194924_create_parties.rb
@@ -3,6 +3,7 @@ class CreateParties < ActiveRecord::Migration[5.2]
     create_table :parties do |t|
       t.string :name
       t.integer :partysize
+      t.text :description
       t.belongs_to :user, foreign_key: true
       t.belongs_to :game, foreign_key: true
 
