142e6e03c26658f3ee7db67f00d88b56053e47cd
WSINTRA <will.simpson85@gmail.com>
2019-05-28 12:39:17 -0400

    Created a party

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,7 +1,10 @@
 class PartiesController < ApplicationController
 
 	def create
-		binding.pry
-		
+		newParty = Party.create(name: params["partyName"], partysize: params["partySize"],
+			description: params["partyDescription"], user_id: params["userId"],
+			game_id: params["gameId"] )
+        user = User.find(params["userId"])
+		render json: user
 	end
 end
