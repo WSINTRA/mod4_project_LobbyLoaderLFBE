a760b1c006598b3b3061aed8546ba88ddbef3967
beschwa <naraiswar@gmail.com>
2019-05-30 13:31:04 -0400

    serialized party

--- app/serializers/party_serializer.rb
+++ app/serializers/party_serializer.rb
@@ -1,7 +1,11 @@
 class PartySerializer < ActiveModel::Serializer
-  attributes :id, :name, :partysize, :description
+  attributes :id, :name, :partysize, :description, :space
   has_one :user
   has_one :game
   has_many :users
   has_many :messages
+
+  def space
+  	{taken: "#{self.object.users.length+1}", available: "#{self.object.partysize-1-self.object.users.length}"}
+  end
 end
